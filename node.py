2f76eef34e3681a3b772bf228b45c989faa8be65
Chamil José Cruz Razeq <chamil@Chamils-MacBook-Pro.local>
2022-10-25 17:18:59 +0100

    setted to default file

--- node.py
+++ node.py
@@ -6,15 +6,7 @@ class Node:
     index = taken = value = room = 0
 
     def __init__(self, index, taken, value, room):
-        self.index = index
-        self.taken = taken
-        self.value = value
-        self.room = room
+        pass
 
     def estimate(self, items):
-        est = 0
-
-        for i in items:
-            est = i.value + est
-            
-        return est - self.value
+        return 0
